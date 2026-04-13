21af5b7a345bf67a69f97bc56278578247000265
tonimusaro-tred <tonimusaro@gmail.com>
2026-04-13 07:04:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-const CACHE_NAME = 'dieta-antonio-v1';
-const urlsToCache = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'dieta-antonio-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['/dieta-antonio/', '/dieta-antonio/index.html']);
+    }).catch(() => {
+      // Fallback silenzioso se i file non sono raggiungibili
+      return Promise.resolve();
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).catch(() => caches.match('/dieta-antonio/index.html'));
+    })
   );
 });
